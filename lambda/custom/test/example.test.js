f485cfc7ee758638b5134060d8a89f42ce4df2ac
Wuvist <wuvist@gmail.com>
2020-08-06 18:09:11 +0800

    Add test for getRoomId

--- lambda/custom/test/example.test.js
+++ lambda/custom/test/example.test.js
@@ -1,6 +1,10 @@
-const { replaceWhitespacesDots } = require('../helperFunctions');
+const { replaceWhitespacesDots, getRoomId } = require('../helperFunctions');
 
 const assert = require('assert');
+var MockAdapter = require("axios-mock-adapter");
+var axios = require("axios");
+var mock = new MockAdapter(axios);
+var hostname = "undefined"
 
 describe('replaceWhitespacesDots Test', () => {
     it('should replace white space', () => {
@@ -9,5 +13,21 @@ describe('replaceWhitespacesDots Test', () => {
 
     it('should keep dot', () => {
         assert.equal(replaceWhitespacesDots("."), ".");
+    });
+});
+
+
+describe('getRoomId Test', () => {
+    it('should get room id', async () => {
+        var roomName = "anime";
+        var roomId = 99;
+
+        mock.onGet(hostname + "/api/v1/channels.info?roomName=" + roomName).reply(200, {
+            channel: { _id: roomId },
+        });
+
+        const result = await getRoomId(roomName, {});
+        console.log(mock.history.get[0].url);
+        assert.equal(result, roomId);
     });
 });
